ca722690c69223e154cdd1e97c6a6bfdb91aeaf4
gedion <117908662+Gedion-01@users.noreply.github.com>
2024-10-28 17:07:33 +0300

    implemented the api handlers + refactors

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/Gedion-01/Go-Crud-Challenge/api"
+	"github.com/Gedion-01/Go-Crud-Challenge/db"
 	"github.com/Gedion-01/Go-Crud-Challenge/server"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
-	srv := server.NewServer()
+	memoryDB := db.NewMemoryDB()
+	listenAddr := flag.String("listen-addr", ":5000", "the listen address of the api server")
+
+	srv := server.NewServer(memoryDB)
+	personHandler := api.NewPersonHandler(memoryDB)
+
+	app := fiber.New()
+	apiv1 := app.Group("/api/v1")
+	apiv1.Put("/person/:id", api.CheckUserExists(memoryDB), personHandler.HandlePutPerson)
+	apiv1.Post("/person", personHandler.HandlePostPerson)
+	apiv1.Get("/person/:id", personHandler.HandleGetPerson)
+	apiv1.Get("/persons", personHandler.HandleGetAllPersons)
+	apiv1.Delete("/person/:id", api.CheckUserExists(memoryDB), personHandler.HandleDeletePerson)
+
+	go func() {
+		if err := app.Listen(*listenAddr); err != nil {
+			fmt.Printf("Error starting server: %v\n", err)
+		}
+	}()
 
 	select {
 	case <-stop:
